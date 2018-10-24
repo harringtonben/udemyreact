b7f33d3451aa6c7d044eba4c863c2ee610d56ba2
Ben Harrington <ben.harrington@ipreo.com>
2018-10-24 15:09:50 -0500

    finished first challenge

--- src/App.js
+++ src/App.js
@@ -2,8 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 import * as ReactDOM from "react-dom";
 
-var template = <p>This is jsx from app.js</p>;
+//JSX - Javascript XML
+var template = (
+    <div>
+        <h1>This is jsx from app.js</h1>
+        <p>this is some info</p>
+        <ol>
+          <li>Item 1</li>
+          <li>Item 2</li>
+        </ol>
+    </div>
+);
 
-ReactDOM.render(template, document.getElementById('root'));
+var template2 = (
+    <div>
+      <h1>Ben Harrington</h1>
+      <p>age: 28</p>
+      <p>location: Nashville</p>
+    </div>
+);
 
-export default template;
+// Create a template 2 variable JSX expression
+// div
+//  h1 -> Ben Harrington
+//  p -> age: 28
+//  p -> location: Nashville
+//  Render template 2 instead of template
+
+ReactDOM.render(template2, document.getElementById('root'));
+
+//export default template;
