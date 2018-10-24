3aaf5e6dcba3e0398280ad86dd4eb06e6eecb18b
Ben Harrington <ben.harrington@ipreo.com>
2018-10-24 15:50:03 -0500

    finished third challenge

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import * as ReactDOM from "react-dom";
 
+//only render the subtitle and p tag around subtitle if subtitle exists -- logical &&
+// render new p tag -- if options.length > 0 "here are your options" "no options" --ternary
+
+
 let app = {
     title: 'Indecision App',
-    subtitle: 'this is an app about indecision, so decide'
+    subtitle: 'this is an app about indecision, so decide',
+    options: ['One', 'Two']
 };
 
 let template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options:' : 'No options'}</p>
         <ol>
           <li>Item 1</li>
           <li>Item 2</li>
@@ -24,11 +30,15 @@ let user = {
     location: 'Nashville,TN'
 };
 
+const getLocation = (userLocation) => {
+  return userLocation ? <p>Location: {userLocation}</p> : undefined;
+};
+
 let template2 = (
     <div>
-      <h1>{user.name}</h1>
-      <p>age: {user.age}</p>
-      <p>location: {user.location}</p>
+      <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
