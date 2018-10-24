7ffe4839aba3a1d60d9bcf6e6676ea8011ac5702
Ben Harrington <ben.harrington@ipreo.com>
2018-10-24 14:48:46 -0500

    initial commit for react course

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as ReactDOM from "react-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+var template = <p>This is jsx from app.js</p>;
 
-export default App;
+ReactDOM.render(template, document.getElementById('root'));
+
+export default template;
